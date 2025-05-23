01f7ae5860a47cbce635fee2e638f24c577a2641
rkboyce <rdb20@pitt.edu>
2025-05-23 13:29:39 -0500

    Made it so that the altered SQL will render within Atlas for easier debugging on query efficiency

--- src/main/java/org/ohdsi/webapi/service/SqlRenderService.java
+++ src/main/java/org/ohdsi/webapi/service/SqlRenderService.java
@@ -11,11 +11,14 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import org.apache.commons.lang3.StringUtils;
+import org.ohdsi.sql.SqlCteRefactor;
 import org.ohdsi.sql.SqlRender;
 import org.ohdsi.sql.SqlTranslate;
 import org.ohdsi.webapi.sqlrender.SourceStatement;
 import org.ohdsi.webapi.sqlrender.TranslatedStatement;
 import org.ohdsi.webapi.util.SessionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -23,6 +26,9 @@ import org.ohdsi.webapi.util.SessionUtils;
  */
 @Path("/sqlrender/")
 public class SqlRenderService {
+    // Create a logger instance for this class
+    private static final Logger logger = LoggerFactory.getLogger(SqlRenderService.class);
+    
     /**
      * Translate an OHDSI SQL to a supported target SQL dialect
      * @param sourceStatement JSON with parameters, source SQL, and target dialect
@@ -33,22 +39,28 @@ public class SqlRenderService {
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public TranslatedStatement translateSQLFromSourceStatement(SourceStatement sourceStatement) {
+	logger.info("SqlRenderService::translateSQLFromSourceStatement entered ");
         if (sourceStatement == null) {
+	    logger.info("SqlRenderService::translateSQLFromSourceStatement sourceStatement is null, returning an empty TranslatedStatement");
             return new TranslatedStatement();
         }
         sourceStatement.setOracleTempSchema(TEMP_DATABASE_SCHEMA_PLACEHOLDER);
+
+	logger.info("SqlRenderService::translateSQLFromSourceStatement calling translatedStatement ");
         return translatedStatement(sourceStatement);
     }
 
     public TranslatedStatement translatedStatement(SourceStatement sourceStatement) {
+	logger.info("SqlRenderService::translatedStatement calling translateSQL");
         return translateSQL(sourceStatement);
     }
 
 
     public static TranslatedStatement translateSQL(SourceStatement sourceStatement) {
-
+	logger.info("SqlRenderService::translateSql entered");
         TranslatedStatement translated = new TranslatedStatement();
         if (sourceStatement == null) {
+	    logger.info("SqlRenderService::translateSql sourceStatement is null, returning an empty TranslatedStatement");	    
             return translated;
         }
 
@@ -60,8 +72,12 @@ public class SqlRenderService {
                     parameters.keySet().toArray(new String[0]),
                     parameters.values().toArray(new String[0]));
 
+	    logger.info("SqlRenderService::translateSql renderedSQL completed. SQL: " + renderedSQL);
+
             translated.setTargetSQL(translateSql( sourceStatement, renderedSQL));
 
+	    logger.info("SqlRenderService::translateSql finished translated.setTargetSQL(translateSql...)");
+	    
             return translated;
         } catch (Exception exception) {
             throw new RuntimeException(exception);
@@ -71,9 +87,27 @@ public class SqlRenderService {
 
     private static String translateSql(SourceStatement sourceStatement, String renderedSQL) {
         if (StringUtils.isEmpty(sourceStatement.getTargetDialect()) || DEFAULT_DIALECT.equals(sourceStatement.getTargetDialect())) {
-            return renderedSQL;
+	    logger.info("SqlRenderService::translateSql(SourceStatement sourceStatement, String renderedSQL) - condition regarding getTargetDialect (" + sourceStatement.getTargetDialect() + ") found to be True.");
+	    if(DEFAULT_DIALECT.equals(sourceStatement.getTargetDialect())){ // implies "sql server" is the dialect
+		if (DO_REFACTOR) {
+		    logger.info("SqlRenderService::translateSql calling translateToCustomVaSql");
+		    renderedSQL = SqlCteRefactor.translateToCustomVaSql(renderedSQL);
+		    logger.info("SqlRenderService::translateSql translateToCustomVaSql returned. New SQL:\n\n" + renderedSQL + "\n\n");
+		}		
+	    } 
+	    return renderedSQL;	    
         }
-        return SqlTranslate.translateSql(renderedSQL, sourceStatement.getTargetDialect(), SessionUtils.sessionId(), sourceStatement.getOracleTempSchema());
-    }
+	
+	String sql = SqlTranslate.translateSql(renderedSQL, sourceStatement.getTargetDialect(), SessionUtils.sessionId(), sourceStatement.getOracleTempSchema());
 
+	if(DEFAULT_DIALECT.equals(sourceStatement.getTargetDialect())){ // implies "sql server" is the dialect
+	    if (DO_REFACTOR) {
+		logger.info("SqlRenderService::translateSql calling translateToCustomVaSql");
+		sql = SqlCteRefactor.translateToCustomVaSql(sql);
+		logger.info("SqlRenderService::translateSql translateToCustomVaSql returned. New SQL:\n\n" + sql + "\n\n");
+	    }
+	}
+        return sql;
+    }
+    private static final boolean DO_REFACTOR = true;
 }
